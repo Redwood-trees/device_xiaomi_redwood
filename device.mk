f404de98aa00bce275248d22fa2a519657923254
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2024-01-27 10:48:10 +0530

    redwood: overlay: Add empty Dolby specific frameworks overlay

--- device.mk
+++ device.mk
@@ -325,6 +325,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    DolbyOverlayRedwood
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Overlays-RRO
