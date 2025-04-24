c8b1a371e3ce570ab458ec07e115fbd36c0b88d0
Thereache <thereache2003@gmail.com>
2025-04-24 16:07:45 +0530

    audio : drop audio things
    Revert "redwood: audio: some audio xml changes"
    
    This reverts commit b106e131420725f80fcf3b40c07ef2ba0b344abb.
    
    Revert "redwood: audio: Remove direct flag for VOIP RX"
    
    This reverts commit 3ee16d540e1151af2c356888c8cbd77d5a940036.
    
    Revert "redwood: cleanup & setup the proprietary-files"
    
    This reverts commit 2c89c16f15962c0884ffe13e68cf62d4a31dac12.
    
    Revert "redwood: audio: Enable 24-bit support for wired headphones"
    
    This reverts commit d120241667909f2e3e23dbc63331aedca50222f7.
    
    Revert "redwood: audio: Use QCOM implementation for audio effects"
    
    This reverts commit 68a5c880ac658266aa8e2696d62f9a37328f1bd8.
    
    Revert "redwood: audio: Add dolby volume listener"
    
    This reverts commit 8731c4bbb245e479778c6b42b1b9f72e1ff0a027.
    
    Revert "redwood: Add support of MIUI Dolby"
    
    This reverts commit 624c817a1c66c5bb62a22282de4c551aabd78848.
    
    Revert "redwood: audio: Bring all stream effects"
    
    This reverts commit 45a5ba81b0af110419f9354171b54d01a4c4f862.
    
    Revert "redwood: audio: Sync with Spatial Audio"
    
    This reverts commit 273c99d2d31f099c2bd37674d58d49938dbed562.
    
    Revert "redwood: audio: Remove FM Tuner from audio input devices"
    
    This reverts commit 828366917d4970ebf78fa1d3fe7703af7fca8223.
    
    Revert "redwood: Import device-specific audio configs"
    
    This reverts commit 5643afc3231d18a1b60b668c5fd1727e20b02a38.

--- device.mk
+++ device.mk
@@ -52,13 +52,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_io_policy.conf \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_yupikidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_platform_info_yupikidp.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_yupikidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths_yupikidp.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_overlay_dynamic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths_overlay_dynamic.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths_overlay_static.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_yupikidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/sound_trigger_mixer_paths_yupikidp.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
@@ -163,41 +157,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     libcrypto_shim
-
-# Dolby
-PRODUCT_PACKAGES += \
-    XiaomiDolby
-
-# Dolby Config File
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
-
-# Dolby MediaCodecs Dependenices
-PRODUCT_PACKAGES += \
-    libcodec2_hidl@1.0.vendor \
-    libcodec2_soft_common.vendor \
-    libstagefright_softomx.vendor \
-    libstagefright_softomx_plugin.vendor
-
-# Dolby Props
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
-    ro.vendor.audio.dolby.dax.version=DAX3_3.6 \
-    ro.vendor.audio.dolby.dax.support=true \
-    ro.vendor.audio.dolby.surround.enable=true
-
-# Dolby Effects Props
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.audio.delta.refresh=true
-
-# MiSound
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.audio.misound.disable=true \
-    ro.vendor.audio.misound.bluetooth.enable=true
-
-# Dolby VNDK libs
-PRODUCT_PACKAGES += \
-    libstagefright_foundation-v33
 
 # Fastbootd
 PRODUCT_PACKAGES += \
